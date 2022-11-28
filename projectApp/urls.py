089b72b2215fa9c00fb87e967ea92b2f2f6301e2
wsber <102464006+wsber@users.noreply.github.com>
2022-11-28 12:30:49 +0800

    工程创建后端接口起步开发

---
+++ projectApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('test', views.test),
+    path('saveProject', views.saveProject),
+    path('saveTask', views.saveTask),
+    path('getTasksFromTheProject', views.getTasksFromTheProject),
+    path('getThisUserProjectList', views.getThisUserProjectList),
+]
