2ff8e85fb6be2204a4b5c33984b382188295d0bf
Yurzi <yurzi@foxmail.com>
2022-12-02 16:45:49 +0800

    implement access control of page object

--- apps/rbac/permissions.py
+++ apps/rbac/permissions.py
@@ -1,7 +1,7 @@
 from django.db.models import QuerySet
 from rest_framework import permissions
 
-from .models import Api, Api_Function, Function, User
+from .models import Api, Function, Page, User
 
 
 def union_permissions(
@@ -26,15 +26,51 @@ def filter_permission_by_status(permissions_list: list[int] | QuerySet) -> list[
     return res
 
 
-def filter_permission_by_type(
-    permissions_list: list[int] | QuerySet, rw_type: list[str]
-) -> list[int]:
-    res = list()
-    for permission in permissions_list:
-        permission_obj = Function.objects.filter(id=permission).first()
-        if permission_obj.rw_type in rw_type:
-            res.append(permission)
-    return res
+# 获取用户对应的角色权限及其特殊权限
+def get_user_permissions(user: User) -> list[int]:
+    user_has_role_permissions = (
+        User.objects.filter(username=user.username)
+        .first()
+        .roles.all()
+        .values_list("role__functions__function_id", flat=True)
+    )
+    user_has_spec_permissions = (
+        User.objects.filter(username=user.username)
+        .first()
+        .functions.all()
+        .values_list("function_id", flat=True)
+    )
+    # print(user_has_spec_permissions)
+    user_has_permissions = union_permissions(
+        user_has_role_permissions, user_has_spec_permissions
+    )
+    user_has_permissions = filter_permission_by_status(user_has_permissions)
+    return user_has_permissions
+
+
+def check_permissions_requirements(
+    user_has_permissions: list[int] | QuerySet,
+    required_permissions: list[int] | QuerySet,
+) -> bool:
+    check_status = False
+    for required in required_permissions:
+        if required in user_has_permissions:
+            check_status = True
+            continue
+        # 如果没有直接找到，则查找其父权限是否存在
+        while True:
+            parent = Function.objects.filter(id=required).first().parent
+            if parent is None:
+                check_status = False
+                break
+            if parent.id in user_has_permissions:
+                check_status = True
+                break
+
+        if not check_status:
+            return check_status
+
+    return check_status
 
 
 class GeneralPermission(permissions.BasePermission):
@@ -55,58 +91,41 @@ class GeneralPermission(permissions.BasePermission):
         if api_obj is None:
             return False
 
-        api_required_functions = api_obj.required_functions.all().values_list(
-            "function_id", flat=True
-        )
-        if request.methond in permissions.SAFE_METHODS:
-            api_required_functions = filter_permission_by_type(
-                api_required_functions, ["r", "a"]
-            )
+        if request.method in permissions.SAFE_METHODS:
+            rw_type = ["r", "a"]
         else:
-            api_required_functions = filter_permission_by_type(
-                api_required_functions, ["w", "a"]
-            )
+            rw_type = ["w", "a"]
+
+        api_required_functions = (
+            api_obj.required_functions.filter(rw_type__in=rw_type)
+            .all()
+            .values_list("function_id", flat=True)
+        )
 
         # 获取用户到权限
         # print(request.user.username)
-
-        user_has_role_permissions = (
-            User.objects.filter(username=request.user.username)
-            .first()
-            .roles.all()
-            .values_list("role__functions__function_id", flat=True)
-        )
-        user_has_spec_permissions = (
-            User.objects.filter(username=request.user.username)
-            .first()
-            .functions.all()
-            .values_list("function_id", flat=True)
-        )
-        # print(user_has_spec_permissions)
-        user_has_permissions = union_permissions(
-            user_has_role_permissions, user_has_spec_permissions
-        )
-        user_has_permissions = filter_permission_by_status(user_has_permissions)
+        user_has_permissions = get_user_permissions(request.user)
 
         # print(user_has_permissions[0])
 
         # 检验权限
-        check_status = False
-        for required in api_required_functions:
-            if required in user_has_permissions:
-                check_status = True
-                continue
-            # 如果没有直接找到，则查找其父权限是否存在
-            while True:
-                parent = Function.objects.filter(id=required).first().parent
-                if parent is None:
-                    check_status = False
-                    break
-                if parent.id in user_has_permissions:
-                    check_status = True
-                    break
+        return check_permissions_requirements(
+            user_has_permissions, api_required_functions
+        )
 
-            if not check_status:
-                break
 
-        return check_status
+class HasAccessOfPage(permissions.BasePermission):
+    message = "You don't have the access to the page"
+
+    def has_object_permission(self, request, view, obj: Page) -> bool:
+        # 获取用户所具有的权限
+        user_has_permissions = get_user_permissions(request.user)
+
+        # 获取页面需要的权限
+        page_required_functions = obj.required_functions.all().values_list(
+            "function_id", flat=True
+        )
+
+        return check_permissions_requirements(
+            user_has_permissions, page_required_functions
+        )
