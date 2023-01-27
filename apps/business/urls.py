c21b3adbb7d6da3c36b8a378193e825fbd20e8e3
OceanPresent <oceanpresent@163.com>
2023-01-27 16:11:25 +0800

    feat: task & record api

--- apps/business/urls.py
+++ apps/business/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import project
+from .views import project,task
 
 urlpatterns = [
     path('/saveProject', project.saveProject),
     path('/getTasksFromTheProject', project.getTasksFromTheProject),
     path('/getThisUserProjectList', project.getThisUserProjectList),
     path('/newProject', project.newProject),
+    path('/task/<int:pk>', task.TaskView.as_view()),
+    path('/record/list', task.RecordListView.as_view()),
+    path('/usertask/list', task.Task_UserListViews.as_view()),
+    path('/task/submit/<int:pk>', task.SubmitTaskView.as_view()),
 ]
--- apps/business/views/task.py
+++ apps/business/views/task.py
@@ -3,8 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView,ListAPIView
 from common.custom_response import CustomResponse
 from common.custom_exception import CustomException
-from ..serializers import TaskSerializer,RecordSerializer
-from ..models import Task,Record,Article
+from ..serializers import TaskSerializer,RecordSerializer,TaskUserSerializer
+from ..models import Task,Record,Article,Task_User
+from ...rbac.models import User
 import uuid
 
 def getSearchObject(query,queries:list[str]):
@@ -14,22 +15,27 @@ def getSearchObject(query,queries:list[str]):
       res[key] = query[key]
   return res
 
-class TaskView(APIView):
+class Task_UserListViews(ListAPIView):
+    queryset = Task_User.objects.all()
+    serializer_class = TaskUserSerializer
 
+    def get_queryset(self):
+      searchObj = getSearchObject(self.request.query_params,['user','duty','task'])
+      queryset = self.queryset.filter(**searchObj)
+      return queryset
+
+class TaskView(APIView):
   def get(self, request:request.Request, pk, format=None):
-    '''获取任务列表'''
-    searchObj = getSearchObject(request.query_params,['user','project'])
-    tasks = Task.objects.all()
-    if 'user' in searchObj:
-      tasks = tasks.filter(members__id=searchObj['user'])
-    if 'project' in searchObj:
-      tasks = tasks.filter(project=searchObj['project'])
-    serializer = TaskSerializer(tasks, many=True)
-    return CustomResponse(data=serializer.data, status=status.HTTP_200_OK, message="获取任务列表成功")
+    '''获取某个task'''
+    task = Task.objects.get(pk=pk)
+    if(task is None):
+      raise CustomException(status_code=status.HTTP_404_NOT_FOUND, message="任务不存在")
+    serializer = TaskSerializer(task)
+    return CustomResponse(data=serializer.data, status=status.HTTP_200_OK, message="获取任务成功")
 
   def put(self, request, pk, format=None):
     '''修改任务'''
-    task = Task.objects.get(pk=request.data["task"])
+    task = Task.objects.get(pk=pk)
     if(task is None):
       raise CustomException(status_code=status.HTTP_404_NOT_FOUND, message="任务不存在")
     serializer = TaskSerializer(task, data=request.data)
@@ -44,25 +50,32 @@ class RecordListView(ListAPIView):
     ordering_fields = ('status',)
 
     def get_queryset(self):
-      searchObj = getSearchObject(self.request.query_params,['user','project','task'])
+      searchObj = getSearchObject(self.request.query_params,['user','project','task','type','status'])
       queryset = self.queryset.filter(**searchObj)
       return queryset
 
 class SubmitTaskView(CreateAPIView):
   def post(self, request, pk, format=None):
     '''提交任务某一个步骤'''
-    task = Task.objects.get(pk=request.data["task"])
+    task = Task.objects.get(pk=pk)
     if(task is None):
       raise CustomException(status_code=status.HTTP_404_NOT_FOUND, message="任务不存在")
     if(task.status == 'f' or task.step >= 5):
       raise CustomException(status_code=status.HTTP_400_BAD_REQUEST, message="任务已经完成")
     step = task.step
     Record.objects.filter(task=task, type=step).update(status='s')
-    Record.objects.create(task=task, user=request.user, content=request.data["content"],type=step,name=uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org'),status='r')
+    if not 'content' in request.data:
+      raise CustomException(status_code=status.HTTP_400_BAD_REQUEST, message="内容不能为空")
+
+    user = User.objects.get(pk=request.data["user"])
+    if user is None:
+      raise CustomException(status_code=status.HTTP_404_NOT_FOUND, message="用户不存在")
+
+    Record.objects.create(task=task, user=user, content=request.data["content"],type=step,name=str(uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org'))[:20],status='r',project=task.project)
 
     task.step = task.step + 1
     task.save(force_update=True)
-    return CustomResponse(status=status.HTTP_200_OK, message="修改任务成功")
+    return CustomResponse(status=status.HTTP_200_OK, message="提交成功")
 
 
 class FinishTaskView(CreateAPIView):
