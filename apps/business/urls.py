dfa9bae7bdb0eb9311bceb3c9b1c17d5d94df0af
OceanPresent <oceanpresent@163.com>
2023-01-29 00:09:19 +0800

    fix: fix url path

--- apps/business/urls.py
+++ apps/business/urls.py
@@ -4,11 +4,11 @@ from .views import project,task
 
 urlpatterns = [
 
-    path('saveProject', project.saveProject),
-    path('getTasksFromTheProject', project.getTasksFromTheProject),
-    path('saveTask', project.saveTask),
-    path('/getThisUserProjectList/', project.getThisUserProjectList),
-    path('/newProject/', project.newProject),
+    path('/saveProject', project.saveProject),
+    path('/getTasksFromTheProject', project.getTasksFromTheProject),
+    path('/saveTask', project.saveTask),
+    path('/getThisUserProjectList', project.getThisUserProjectList),
+    path('/newProject', project.newProject),
     path('/task/<int:pk>', task.TaskView.as_view()),
     path('/record/list', task.RecordListView.as_view()),
     path('/usertask/list', task.Task_UserListViews.as_view()),
