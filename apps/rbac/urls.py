c8f6f47cffb89b48d2c97cbe20ca3f578ad308fe
Xiaoxuan <git config --global user.email 2720799009@qq.com>
2023-01-29 01:23:49 +0800

    实现了分页获取角色列表删除多个用户

--- apps/rbac/urls.py
+++ apps/rbac/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 from .views.user import UserListView, UserDetailView, UserRegisterView, login
-
 from .views.Role import Role_FunctionView
 
 urlpatterns = [
@@ -11,5 +10,10 @@ urlpatterns = [
     path('/user/list', UserListView.as_view(), name='user_list'),
     path('/user/login', login, name='user_login'),
     path('/permission/role/delfunction/',Role_FunctionView.as_view()),
-    path('/permission/role/updfunction/',Role_FunctionView.as_view())
+    path('/permission/role/updfunction/',Role_FunctionView.as_view()),
+    path('/user/ids/', WsUserView.as_view()),
+    path('/permission/role', Role_FunctionView.as_view()),
+    path('/permission/role/updfunction/', Role_FunctionView.as_view()),
+    path('/permisssion/role/list/', Role_FunctionView.as_view()),
+
 ]
--- apps/rbac/views/Role.py
+++ apps/rbac/views/Role.py
@@ -7,12 +7,21 @@ from ..models import Function
 from rest_framework import views
 from rest_framework import serializers
 import traceback
+from django.core.paginator import Paginator, EmptyPage
 #Role_Function序列化器
 class R_FSer(serializers.ModelSerializer):
     class Meta:
         model = Role_Function
         fields = '__all__'
         read_only_fields = ('addTime',)
+class ZqxRSer(serializers.Serializer):
+    roleId = serializers.IntegerField(source = "id")
+    rolename = serializers.CharField(max_length = 30,source = "name")
+    desc = serializers.CharField(max_length = 100)
+    addTime = serializers.DateTimeField()
+    status = serializers.CharField()
+
+
 #Role_Function删除
 class Role_FunctionView(views.APIView):
     def delete(self, request):
@@ -27,6 +36,7 @@ class Role_FunctionView(views.APIView):
             return CustomResponse(data=None,message= '删除成功')
         else:
             return CustomResponse(message="该角色权限不存在", code=402)
+    #Role_Function更新
     def put(self,request):
         re_data = request.data
         rid = re_data['roleId']
@@ -53,4 +63,29 @@ class Role_FunctionView(views.APIView):
         for f in fList:
             Role_Function.objects.create(role_id = rid,function_id = f)
         return CustomResponse(data= None,message="更新成功",code=200)
-        
+    def get(self,request):
+        re_data = request.data
+        rolename = re_data['roleName']
+        ps = re_data['pageSize']
+        pn = re_data['pageNum']
+        if rolename is None:
+            roles = Role.objects.all()
+        else:
+            roles = Role.objects.filter(name__contains = rolename).order_by('id')
+        rsize = roles.count()
+        if ps==0 or pn==0 or rsize==0:
+            rt_data =   {"records":[],
+                         "total":rsize
+                        }
+            return CustomResponse(code=200,data = rt_data,message = '查找成功')
+
+        pageinator = Paginator(roles,ps)
+        try:
+            page=pageinator.page(pn)
+        except EmptyPage:
+            page = pageinator.page(1)
+        rolesser = ZqxRSer(instance=page,many = True)
+        rt_data =   {"records":rolesser.data,
+                    "total":rsize
+                    }
+        return CustomResponse(code=200,data = rt_data,message = '查找成功')
