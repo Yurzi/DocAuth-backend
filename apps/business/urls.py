1d0cd5c5f361fab8e78bf95349feaaf6955f6efc
wsber <102464006+wsber@users.noreply.github.com>
2023-01-28 16:13:46 +0800

    完善浏览项目功能

--- apps/business/urls.py
+++ apps/business/urls.py
@@ -5,6 +5,7 @@ from .views import project
 urlpatterns = [
     path('saveProject', project.saveProject),
     path('getTasksFromTheProject', project.getTasksFromTheProject),
+    path('saveTask', project.saveTask),
     path('/getThisUserProjectList/', project.getThisUserProjectList),
     path('/newProject/', project.newProject),
 ]
