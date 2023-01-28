8ddda00556ea6458da1765381a7cbc85d14ff93c
Xiaoxuan <git config --global user.email 2720799009@qq.com>
2023-01-28 17:28:27 +0800

    增加了角色权限删除

--- apps/rbac/urls.py
+++ apps/rbac/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from .views.user import UserListView, UserDetailView, UserRegisterView, login
 from .views.test import test
-
+from .views.Role import Role_FunctionView
 
 urlpatterns = [
     path('/user/',
@@ -11,4 +11,5 @@ urlpatterns = [
     path('/user/list/', UserListView.as_view(), name='user_list'),
     path('/user/login/', login, name='user_login'),
     path('/test/', test, name='test'),
+    path('/permission/role/delfunction/',Role_FunctionView.as_view()),
 ]
