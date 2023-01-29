aedc5c291780e0721ed3abb13f85961f532993c3
Yurzi <yurzi@foxmail.com>
2023-01-29 17:45:28 +0800

    [ExpectionSystem] make CustomExpection fit with framework

--- common/custom_exception.py
+++ common/custom_exception.py
@@ -1,29 +1,28 @@
-from rest_framework.exceptions import APIException
 from rest_framework.views import Response, exception_handler, status
 
 
 # 自定义错误
-class CustomException(APIException):
-    _default_code = "custom_internal_server_error"
+class CustomException(Exception):
+    _default_code = 400
 
     def __init__(
         self,
-        message: str = "A CustomException raised",
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        message: str = "",
+        status_code=status.HTTP_200_OK,
         data=None,
-        code: str = _default_code,
+        code: int = _default_code,
     ):
 
-        self.default_code = code
-        self.status_code = status_code
-        self.default_detail = message
+        self.code = code
+        self.status = status_code
+        self.message = message
         if data is None:
             self.data = {"detail": message}
         else:
             self.data = data
 
     def __str__(self):
-        return self.default_detail
+        return self.message
 
 
 # 自定义错误处理
@@ -32,11 +31,11 @@ def custom_exception_handler(exc, context):
     if isinstance(exc, CustomException):
         response = Response(
             data={
-                "code": exc.default_code,
-                "message": exc.default_detail,
+                "code": exc.code,
+                "message": exc.message,
                 "data": exc.data,
             },
-            status=exc.status_code,
+            status=exc.status,
         )
     else:
         response = Response(
