bbcfa3c07608ae0e5f10af3fb9e3f126615e0e6e
Yurzi <yurzi@foxmail.com>
2023-01-30 00:44:56 +0800

    [FunctionSystem] finish Function operation Api

--- apps/rbac/urls.py
+++ apps/rbac/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, re_path
 from .views.user import UserListView, UserDetailView, UserRegisterView,WsUserView,WsUser_2_view, login
-#from .views.test import test
 from .views.Role import Role_FunctionView,zqxR_F1View,zqx_RView,WRoleView
 from .views.RoleUser import wsRUView
 from .views.UserFunction import W_U_FView,W_U_FViewAll
-from apps.rbac.views.function import FunctionListView
+from apps.rbac.views.function import FunctionListView, FunctionDetailView
 from apps.rbac.views.Role import Role_FunctionView, zqx_RView, zqxR_F1View
 from apps.rbac.views.user import (UserDetailView, UserListView,
                                   UserRegisterView, WsUserView, login)
@@ -16,12 +15,12 @@ urlpatterns = [
     path('/user/list/', UserListView.as_view(), name='user_list'),
     path('/user/login/', login, name='user_login'),
     path('/user/ids/',WsUserView.as_view()),
-    
+
     path('/user/page/',WsUserView.as_view()),
     path('/user/upduser',WsUserView.as_view()),
     path('/permission/user/role/',wsRUView.as_view()),
     path('/user/like/',WsUser_2_view.as_view()),
-    #path('/test/', test, name='test'),
+
     path('/permission/role/delfunction/',Role_FunctionView.as_view()),
     path('/permission/role/updfunction/',Role_FunctionView.as_view()),
     path('/permission/role/list/',Role_FunctionView.as_view()),
@@ -37,4 +36,5 @@ urlpatterns = [
     path("/permission/role", Role_FunctionView.as_view()),
     ## Function
     path("/permission", FunctionListView.as_view()),
+    path("/permission/status", FunctionDetailView.as_view()),
 ]
--- apps/rbac/views/function.py
+++ apps/rbac/views/function.py
@@ -2,6 +2,7 @@ from rest_framework import status, views
 
 from apps.rbac.models import Function
 from apps.rbac.serializers.function_serializer import FunctionSerializer
+from apps.rbac.serializers.utils import check_serializer_valid
 from common.custom_exception import CustomException
 from common.custom_response import CustomResponse
 
@@ -14,8 +15,10 @@ class FunctionListView(views.APIView):
     def get(self, request):
         # 获取权限列表
         function_list = Function.objects.all()
+        if not function_list.exists():
+            return CustomResponse(status=status.HTTP_200_OK, data={}, message="获取成功")
 
-        serializer = FunctionSerializer(isinstance=function_list)
+        serializer = FunctionSerializer(instance=function_list)
 
         return CustomResponse(
             code=status.HTTP_200_OK, data=serializer.data, message="获取成功"
@@ -24,14 +27,52 @@ class FunctionListView(views.APIView):
     def post(self, request):
         serializer = FunctionSerializer(data=request.data)
         # 校验数据
-        if not serializer.is_valid():
-            raise CustomException(
-                message="格式错误",
-                status_code=status.HTTP_502_BAD_GATEWAY,
-                code=status.HTTP_502_BAD_GATEWAY,
-                data=serializer.errors,
-            )
+        check_serializer_valid(serializer)
         # 校验通过
         serializer.save()
         # 返回
         return CustomResponse(code=status.HTTP_200_OK, data=serializer.data)
+
+
+class FunctionDetailView(views.APIView):
+    """
+    For Function Resource
+    """
+
+    def get_object(self, pk: int):
+        try:
+            function = Function.objects.get(id=pk)
+        except Function.DoesNotExist:
+            raise CustomException(
+                code=status.HTTP_404_NOT_FOUND,
+                status_code=status.HTTP_404_NOT_FOUND,
+                message="对象未找到",
+                data={"id": pk},
+            )
+        return function
+
+    def get(self, request):
+        # 获取请求数据
+        request_dict = request.query_params
+        function_id = request_dict["id"]
+
+        serializer = FunctionSerializer(instance=self.get_object(pk=function_id))
+        return CustomResponse(code=status.HTTP_200_OK, data=serializer.data)
+
+    def put(self, request):
+        serializer = FunctionSerializer(data=request.data)
+        check_serializer_valid(serializer)
+        serializer.save()
+        return CustomResponse(code=status.HTTP_200_OK, data=serializer.data)
+
+    def delete(self, request):
+        request_dict = request.query_params
+        function_id = request_dict["id"]
+
+        function = self.get_object(pk=function_id)
+        if not function.exists():
+            return CustomResponse(
+                code=status.HTTP_202_ACCEPTED, message="对象不存在", data={}
+            )
+        function.delete()
+        return CustomResponse(code=status.HTTP_200_OK, message="删除成功", data={})
