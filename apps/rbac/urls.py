00515829957b6fe9f4746b277b011d4dbffeac88
Xiaoxuan <git config --global user.email 2720799009@qq.com>
2023-01-29 14:15:44 +0800

    目前的角色权限列表接口初步完成

--- apps/rbac/urls.py
+++ apps/rbac/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from .views.user import UserListView, UserDetailView, UserRegisterView,WsUserView, login
 #from .views.test import test
-from .views.Role import Role_FunctionView
+from .views.Role import Role_FunctionView,zqxR_F1View,zqx_RView
 
 urlpatterns = [
     path('/user/',
@@ -12,8 +12,14 @@ urlpatterns = [
     path('/user/login/', login, name='user_login'),
     path('/user/ids/',WsUserView.as_view()),
     #path('/test/', test, name='test'),
-    path('/permission/role',Role_FunctionView.as_view()),
+    path('/permission/role/',Role_FunctionView.as_view()),
     path('/permission/role/updfunction/',Role_FunctionView.as_view()),
-    path('/permisssion/role/list/',Role_FunctionView.as_view()),
+    path('/permission/role/list/',Role_FunctionView.as_view()),
+    path('/permission/role/add/',Role_FunctionView.as_view()),
+    path('/permission/role/oneRoleList/',zqxR_F1View.as_view()),
+    path('/permission/role/ids/',zqxR_F1View.as_view()),
+    path('/permission/role/upInfo/',zqxR_F1View.as_view()),
+    path('/permission/role/delOne/',zqx_RView.as_view()),
+    path('/permission/role/upstatus/',zqx_RView.as_view()),
 
 ]
