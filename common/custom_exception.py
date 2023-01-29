f9d23fca6358b2163ce5629d5704b319c1115da9
Yurzi <yurzi@foxmail.com>
2023-01-29 17:28:38 +0800

    [ExpectionSystem] make CustomExpection fit with framework

--- common/custom_exception.py
+++ common/custom_exception.py
@@ -1,25 +1,30 @@
-from rest_framework.views import exception_handler,Response,status
+from rest_framework.exceptions import APIException
+from rest_framework.views import Response, exception_handler, status
+
+
 # 自定义错误
-class CustomException(Exception):
-    _default_code = 400
+class CustomException(APIException):
+    _default_code = "custom_internal_server_error"
+
     def __init__(
         self,
-        message: str = "",
-        status_code=status.HTTP_200_OK,
+        message: str = "A CustomException raised",
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         data=None,
-        code: int = _default_code,
+        code: str = _default_code,
     ):
 
-        self.code = code
-        self.status = status_code
-        self.message = message
+        self.default_code = code
+        self.status_code = status_code
+        self.default_detail = message
         if data is None:
             self.data = {"detail": message}
         else:
             self.data = data
 
     def __str__(self):
-        return self.message
+        return self.default_detail
+
 
 # 自定义错误处理
 def custom_exception_handler(exc, context):
@@ -27,11 +32,11 @@ def custom_exception_handler(exc, context):
     if isinstance(exc, CustomException):
         response = Response(
             data={
-                "code": exc.code,
-                "message": exc.message,
-                'data':exc.data,
+                "code": exc.default_code,
+                "message": exc.default_detail,
+                "data": exc.data,
             },
-            status=exc.status,
+            status=exc.status_code,
         )
     else:
         response = Response(
@@ -42,4 +47,3 @@ def custom_exception_handler(exc, context):
             status=400,
         )
     return response
-
