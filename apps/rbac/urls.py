5e008c685d9081206ed89476331f2ed3d7f641d4
Yurzi <yurzi@foxmail.com>
2023-01-29 21:35:36 +0800

    [FunctionSystem] implement FunctionListView

--- apps/rbac/urls.py
+++ apps/rbac/urls.py
@@ -1,24 +1,26 @@
 from django.urls import path, re_path
-from .views.user import UserListView, UserDetailView, UserRegisterView, login
-from .views.Role import Role_FunctionView
+
+from apps.rbac.views.function import FunctionListView
+from apps.rbac.views.Role import Role_FunctionView, zqx_RView, zqxR_F1View
+from apps.rbac.views.user import (UserDetailView, UserListView,
+                                  UserRegisterView, WsUserView, login)
 
 urlpatterns = [
-    path('/user',
-         UserRegisterView.as_view(), name='user_create'),
-    path('/user/<int:pk>',
-         UserDetailView.as_view(), name='user_detail'),
-    path('/user/list', UserListView.as_view(), name='user_list'),
-    path('/user/login', login, name='user_login'),
-    path('/permission/role/delfunction/',Role_FunctionView.as_view()),
-    path('/permission/role/updfunction/',Role_FunctionView.as_view()),
-    path('/user/ids/', WsUserView.as_view()),
-    path('/permission/role', Role_FunctionView.as_view()),
-    path('/permisssion/role/list/', Role_FunctionView.as_view()),
-    path('/permission/role/add/',Role_FunctionView.as_view()),
-    path('/permission/role/oneRoleList/',zqxR_F1View.as_view()),
-    path('/permission/role/ids/',zqxR_F1View.as_view()),
-    path('/permission/role/upInfo/',zqxR_F1View.as_view()),
-    path('/permission/role/delOne/',zqx_RView.as_view()),
-    path('/permission/role/upstatus/',zqx_RView.as_view()),
-
+    path("/user", UserRegisterView.as_view(), name="user_create"),
+    path("/user/<int:pk>", UserDetailView.as_view(), name="user_detail"),
+    path("/user/list", UserListView.as_view(), name="user_list"),
+    path("/user/login", login, name="user_login"),
+    path("/permission/role/delfunction/", Role_FunctionView.as_view()),
+    path("/permission/role/updfunction/", Role_FunctionView.as_view()),
+    path("/user/ids/", WsUserView.as_view()),
+    path("/permission/role", Role_FunctionView.as_view()),
+    path("/permisssion/role/list/", Role_FunctionView.as_view()),
+    path("/permission/role/add/", Role_FunctionView.as_view()),
+    path("/permission/role/oneRoleList/", zqxR_F1View.as_view()),
+    path("/permission/role/ids/", zqxR_F1View.as_view()),
+    path("/permission/role/upInfo/", zqxR_F1View.as_view()),
+    path("/permission/role/delOne/", zqx_RView.as_view()),
+    path("/permission/role/upstatus/", zqx_RView.as_view()),
+    ## Function
+    path("/permission/", FunctionListView.as_view()),
 ]
--- apps/rbac/views/function.py
+++ apps/rbac/views/function.py
@@ -1,6 +1,7 @@
-from rest_framework import views
+from rest_framework import status, views
 
 from apps.rbac.models import Function
+from apps.rbac.serializers.function_serializer import FunctionSerializer
 from common.custom_exception import CustomException
 from common.custom_response import CustomResponse
 
@@ -11,4 +12,26 @@ class FunctionListView(views.APIView):
     """
 
     def get(self, request):
-        raise CustomException
+        # 获取权限列表
+        function_list = Function.objects.all()
+
+        serializer = FunctionSerializer(isinstance=function_list)
+
+        return CustomResponse(
+            code=status.HTTP_200_OK, data=serializer.data, message="获取成功"
+        )
+
+    def post(self, request):
+        serializer = FunctionSerializer(data=request.data)
+        # 校验数据
+        if not serializer.is_valid():
+            raise CustomException(
+                message="格式错误",
+                status_code=status.HTTP_502_BAD_GATEWAY,
+                code=status.HTTP_502_BAD_GATEWAY,
+                data=serializer.errors,
+            )
+        # 校验通过
+        serializer.save()
+        # 返回
+        return CustomResponse(code=status.HTTP_200_OK, data=serializer.data)
