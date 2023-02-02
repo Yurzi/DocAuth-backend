886994ffbd3343c892afc4bfd12ffbc4c222c536
wsber <102464006+wsber@users.noreply.github.com>
2023-02-02 15:50:43 +0800

    完善浏览项目功能

--- apps/business/views/project.py
+++ apps/business/views/project.py
@@ -101,20 +101,25 @@ def saveProject(request):
 
 # 根据当前taskid,更新有关该task所有信息
 def saveTask(request):
+    # post请求参数体中包含该任务重新分配后的信息
     data = json.loads(request.body)
-    print(data)
+    # print(data)
+    # 获取该任务Id
     currentTaskId = data["id"]
+    # 获取该任务最新的职员分配信息（用户id数组）
     staffs = data["staffs"]
-    print(staffs)
+    # print(staffs)
     # 删除数据库task_user表中与当前task相关的所有user
     Task_User.objects.filter(task_id=currentTaskId).delete()
-    # 用新数据更新task表以及在task_user新增重新分配的user关系字段
+    # 用新数据更新task表
     Task.objects.filter(id=currentTaskId).update(name=data["name"], startTime=data["startTime"],
                                                  deadLine=data["deadLine"], desc=data["desc"])
     duty = 1
+    # 根据任务id获取当前任务对像
     taskTmp = Task.objects.get(pk=data["id"])
-
+    # 在task_user表中新增重新分配的user关系字段
     for staff in staffs:
+        # 根据用户id,获得此用户id的用户对象
         userTmp = User.objects.get(pk=staff)
         Task_User.objects.create(addTime=datetime.datetime.now(), duty=duty, task=taskTmp, user=userTmp)
         duty += 1
@@ -132,17 +137,23 @@ def getThisUserProjectList(request):
 
 # 根据项目的pid得到该项目下的任务列表以及各任务对应的人员分配信息
 def getTasksFromTheProject(request):
-    print(request.GET)
+    # print(request.GET)
+    # 获得前端传递的项目id
     projectId = request.GET.get("projectId")
+    # 根据项目id获得当前的项目对象（id，name,desc,status，阶段数量等信息）
     currentProject = Project.objects.get(pk=projectId)
-    print(projectId)
-    print(currentProject.phaseNumber)
+    # print(projectId)
+    # print(currentProject.phaseNumber)
+    # 当前项目的阶段数量
     phases = currentProject.phaseNumber
     # phases = 2
     # projectInfo = {}
+    # 阶段列表
     phaseList = []
 
+    # 遍历每个阶段
     for phase in range(1, phases + 1):
+        # 根据当前项目和阶段号找到该阶段的所有Task，保存在TaskliSt中
         TaskList = Task.objects.filter(project=currentProject, phase=phase).values(
             "id",
             "name",
@@ -153,20 +164,24 @@ def getTasksFromTheProject(request):
             "desc",
             "deadLine",
             "startTime")
+        # 初始化阶段数据:阶段名,任务数两
         phaseItem = {}
         phaseItem["phaseName"] = "Phase " + str(phase)
         phaseItem["task__number"] = len(TaskList)
         userList = []
+        # 保存该阶段每个任务所负责的职员信息:"user_id", "user__username", "duty"
         applierList = []
-        # 下面是默认两个阶段，每个阶段有若干个任务
+        # 每个阶段有若干个任务,获得各任务职员信息
         for task in TaskList:
             applierList.append(
                 list(Task_User.objects.filter(task_id=task['id']).values("user_id", "user__username", "duty")))
         TaskList = list(TaskList)
+        # 在TaskList中添加新key - 分配人员
         for i in range(len(TaskList)):
             TaskList[i]['AssignedPersons'] = applierList[i]
         # print(applierList)
         # print(TaskList)
+        # 将当前阶段的任务列表保存在phaseItem的key=phaseTasks中
         phaseItem["phaseTasks"] = TaskList
         phaseList.append(phaseItem)
 
